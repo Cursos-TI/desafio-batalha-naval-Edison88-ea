2c830ef932be2ea220c9b1f60160271177a5968e
Edison de Almeida <edi_almeida2011@hotmail.com>
2025-04-07 13:10:03 +0000

    commit "Nível mestre"

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -15,8 +15,22 @@ int main() {
         }
     }
 
+    // NAVIOS
+    // Adiciona 2 navios na vertical (tamanho 3)
+     for (i = 2; i <= 4; i++) {
+        tabuleiro[i][3] = 3; //  coluna D, linhas 3-5
+        tabuleiro[i][7] = 3; // coluna H, linhas 3-5
+    }
 
-     // Aplica habilidade CONE 
+    // Adiciona 2 navios na horizontal (tamanho 3)
+    for (j = 1; j <= 3; j++) {
+        tabuleiro[5][j] = 3; //  linha 6, colunas B-D
+        tabuleiro[8][j] = 3; //  linha 9, colunas B-D
+    }   
+
+
+    // HABILIDADES
+    // Aplica habilidade CONE 
      for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
             // Verifica se está na área do cone
@@ -43,31 +57,13 @@ int main() {
     // Aplica habilidade OCTAEDRO 
     for (i = 0; i < 10; i++) {
         for (j = 0; j < 10; j++) {
-            // Verifica se está na área do octaedro (losango)
-            int dist = abs(i - 8) + abs(j - 2);  
-            if (dist <= 2) {  
-                tabuleiro[i][j] = 7;  
-            }
+            int dist = (i > 8 ? i - 8 : 8 - i) + (j > 2 ? j - 2 : 2 - j);
+            if (dist <= 2) {
+                tabuleiro[i][j] = 7;
+}
         }    
     }
             
-
-        // Imprime o tabuleiro
-        printf("~~~~~~~~~ BATALHA NAVAL ~~~~~~~~~\n");
-        printf("\n");
-        printf("   ");
-        for (j = 0; j < 10; j++){
-            printf(" %c ", linha[j]); // imprime a coluna das letras
-        }
-        printf("\n");
-        
-        for (i = 0; i < 10; i++) {
-            printf("%2d ", i + 1); //Imprime número das linhas
-            for (j = 0; j < 10; j++) {
-                printf("%2d ", tabuleiro[i][j]); // imprime o tabuleiro
-            }
-            printf("\n");
-        }
     
      // Imprime o tabuleiro
      printf("~~~~~~~~~ BATALHA NAVAL ~~~~~~~~~\n");
@@ -83,12 +79,14 @@ int main() {
          for (j = 0; j < 10; j++) {
              if (tabuleiro[i][j] == 0) {
                  printf(" 0 "); // Água
-             } else if (tabuleiro[i][j] == 5) {
+             }else if (tabuleiro[i][j] == 3) {
+                printf(" 3 "); // Navio
+            } else if (tabuleiro[i][j] == 5) {
                  printf(" 1 "); // Área afetada por habilidade cone
              } else if (tabuleiro[i][j] == 6) {
                 printf(" 2 "); // Área afetada por habilidade cruz
             }  else if (tabuleiro[i][j] == 7) {
-                printf(" 3 "); // Área afetada por habilidade octaedro
+                printf(" 4 "); // Área afetada por habilidade octaedro
             } 
          }
          printf("\n");
